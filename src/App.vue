835f002ee55924d032c377f0ba395b1136c0dd0a
Alan Gatt <alan@lobeslab.com>
2021-02-24 10:48:51 +0100

    Displaying lists

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <page-home></page-home>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PageHome: PageHome
   }
 }
 </script>
